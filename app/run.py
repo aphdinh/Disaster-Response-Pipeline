937ace021e89f5017dbcb5ea24d13a30f6eade1f
aphdinh <56706731+aphdinh@users.noreply.github.com>
2021-05-06 09:52:07 +0700

    Update run.py

--- app/run.py
+++ app/run.py
@@ -30,7 +30,7 @@ engine = create_engine('sqlite:///../data/YourDatabaseName.db')
 df = pd.read_sql_table('YourTableName', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
